521d390359b7274a2934911ef4bbe86fef056a6c
Валентин Борыш <valentin.b@nekidaem.ru>
2022-07-28 23:42:20 +0300

    Начал описывать модели

--- backend/src/recipes/models.py
+++ backend/src/recipes/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        model=User,
+        related_name='recipes',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    image = models.ImageField()
+    text = models.TextField()
+    cooking_time = models.FloatField()
+
+
+class Ingredient(models.Model):
+    GRAM = 'гр'
+    KILOGRAM = 'кг'
+    MEASUREMENT_UNIT_CHOICES = [
+        (GRAM, 'граммы'),
+        (KILOGRAM, 'килограммы')
+    ]
+    name = models.CharField(max_length=255)
+    measurement_unit = models.CharField(
+        max_length=2,
+        choices=MEASUREMENT_UNIT_CHOICES,
+        default=GRAM
+    )
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    hex_code = models.CharField(max_length=7)
+    slug = models.CharField(max_length=16)
+    
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        model=Recipe,
+        related_name='ingredients',
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        model=Ingredient,
+        related_name='recipes',
+        on_delete=models.SET_NULL
+    )
--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Subscribe(models.Model):
+    subscriber = models.ForeignKey(
+        model=User,
+        related_name='subscribes',
+        on_delete=models.CASCADE
+    )
+    subscribed_for = models.ForeignKey(
+        model=User,
+        related_name='subscribes',
+        on_delete=models.CASCADE
+    )
