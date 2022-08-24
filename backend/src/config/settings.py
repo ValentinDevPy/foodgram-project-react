5e5970d4e758989a7bd01510d32f72c140380434
Валентин Борыш <valentin.b@nekidaem.ru>
2022-08-24 19:19:00 +0300

    Фикс

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -14,7 +14,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "158.160.2.224", "localhost"]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-AUTH_USER_MODEL = User
+AUTH_USER_MODEL = "users.User"
 
 STATIC_URL = "/django_static/"
 MEDIA_URL = "/django_media/"
